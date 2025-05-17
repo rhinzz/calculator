b174d20a1cfe7ab6778f9da296aba9425b20b946
rhinzz <prama.sudjana@gmail.com>
2025-05-17 10:31:45 +0700

    Create base calculator function

---
+++ script.js
@@ -0,0 +1,41 @@
+let num1;
+let num2;
+let operator;
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+console.log(add(5, 4));
+console.log(subtract(5, 4));
+console.log(multiply(5, 4));
+console.log(divide(5, 4));
+
+function operate(num1, num2, operator) {
+    switch (operator) {
+        case "+":
+            return add(num1, num2);
+        case "-":
+            return subtract(num1, num2);
+        case "*":
+            return multiply(num1, num2);
+        case "/":
+            return divide(num1, num2);
+        default:
+            return "Invalid Operator!";
+    }
+}
+
+console.log(operate(3, 4, "+"));
