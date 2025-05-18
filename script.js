9106ed79ac8f6993579f083f40402bddbef31a6e
rhinzz <prama.sudjana@gmail.com>
2025-05-18 20:51:56 +0700

    Create basic calculator functions and operations

--- script.js
+++ script.js
@@ -1,6 +1,66 @@
-let num1;
-let num2;
-let operator;
+// Global Variable
+let numInput1;
+let numInput2;
+let operatorInput;
+
+document.querySelectorAll(".buttonRow button").forEach(button => {
+    button.addEventListener("click", e => {
+        let value = e.target.value;
+        if (document.querySelector(".displayContainer").textContent === "0") {
+            document.querySelector(".displayContainer").textContent = value;
+        } else {
+            document.querySelector(".displayContainer").textContent += value;
+          }
+    })
+});
+
+document.querySelector("#sum").addEventListener("click", () => {
+    numInput1 = +document.querySelector(".displayContainer").textContent;
+    operatorInput = document.querySelector("#sum").textContent;
+    document.querySelector(".displayContainer").textContent = null; 
+});
+
+document.querySelector("#subtract").addEventListener("click", () => {
+    numInput1 = +document.querySelector(".displayContainer").textContent;
+    operatorInput = document.querySelector("#subtract").textContent;
+    document.querySelector(".displayContainer").textContent = null;
+});
+
+document.querySelector("#multiply").addEventListener("click", () => {
+    numInput1 = +document.querySelector(".displayContainer").textContent;
+    operatorInput = document.querySelector("#multiply").textContent;
+    document.querySelector(".displayContainer").textContent = null;
+});
+
+document.querySelector("#divide").addEventListener("click", () => {
+    numInput1 = +document.querySelector(".displayContainer").textContent;
+    operatorInput = document.querySelector("#divide").textContent;
+    document.querySelector(".displayContainer").textContent = null;
+});
+
+document.querySelector("#result").addEventListener("click", () => {
+    numInput2 = +document.querySelector(".displayContainer").textContent;
+    document.querySelector(".displayContainer").textContent = operate(numInput1, numInput2, operatorInput); 
+});
+
+document.querySelector("#clear").addEventListener("click", () => {
+    document.querySelector(".displayContainer").textContent = "0"
+});
+
+function operate(num1, num2, operator) {
+    switch (operator) {
+        case "+":
+            return add(num1, num2);
+        case "-":
+            return subtract(num1, num2);
+        case "*":
+            return multiply(num1, num2);
+        case "/":
+            return divide(num1, num2);
+        default:
+            return "Invalid Operator!";
+    }
+}
 
 function add(num1, num2) {
     return num1 + num2;
@@ -16,26 +76,6 @@ function multiply(num1, num2) {
 
 function divide(num1, num2) {
     return num1 / num2;
-}
-
-// console.log(add(5, 4));
-// console.log(subtract(5, 4));
-// console.log(multiply(5, 4));
-// console.log(divide(5, 4));
-
-function operate(num1, num2, operator) {
-    switch (operator) {
-        case "+":
-            return add(num1, num2);
-        case "-":
-            return subtract(num1, num2);
-        case "*":
-            return multiply(num1, num2);
-        case "/":
-            return divide(num1, num2);
-        default:
-            return "Invalid Operator!";
-    }
 }
 
 // console.log(operate(3, 4, "+"));
